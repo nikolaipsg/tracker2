0ed3a3d4f0eaab4588ed7f83f9b041bc089cf39a
nikolaipsg <privet.nikolai@gmail.com>
2025-08-23 22:21:13 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+
+const CACHE="tt-cache-hig-v13-video-only";
+const ASSETS=["/","/index.html","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{let cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp}).catch(()=>r)))});
